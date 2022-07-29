48e70be8fef7ab31d70a25eaba1a868101b0cbcd
houng-borey <houngboreyrupp@gmail.com>
2022-07-29 23:06:38 +0700

    init

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/kh-scott-coder/demo-graphql-golang/graph"
+	"github.com/kh-scott-coder/demo-graphql-golang/graph/generated"
+	"github.com/kh-scott-coder/demo-graphql-golang/internal/auth"
+	database "github.com/kh-scott-coder/demo-graphql-golang/internal/pkg/db/mysql"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+	database.InitDB()
+	defer database.CloseDB()
+	//database.Migrate()
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// http.Handle("/query", srv)
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
